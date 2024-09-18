4aa307ab7aa71631f701b3f93b8c2e7782587a68
gauravpdwl <gauravpadwal45@gmail.com>
2024-09-18 09:11:28 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res, next) => {
+  const err = createHttpError(401, "You are not allowed to access");
+  return next(err);
   res.send("Welcome to Workquest Auth Service");
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 
 export default app;
